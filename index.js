e10d49a05c42ee9f5ea517207e7ce71b7a2cfcbf
pet0cek <152738828+pet0cek@users.noreply.github.com>
2024-01-13 21:17:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const steamUser = require('steam-user');
-const steamTotp = require('steam-totp');
+const steamUser = require('steam-user')
+const steamTotp = require('steam-totp')
 const keep_alive = require('./keep_alive.js')
 
-var username = process.env.username;
-var password = process.env.password;
-var shared_secret = process.env.shared;
+var username = process.env.username
+var password = process.env.password
+var shared_secret = process.env.shared
 
-var games = [[271590, 105600, 730, 4000, 1118200];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [[271590, 105600, 730, 4000, 1118200]  // Enter here AppIDs of the needed games
+var status = 1  // 1 - online, 7 - invisible
 
 
-user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user = new steamUser()
+user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)})
 user.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-	user.setPersona(status);               
-	user.gamesPlayed(games);
-});
+	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on')
+	user.setPersona(status)               
+	user.gamesPlayed(games)
+})
