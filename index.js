1ac2d5d1c76221ce5c562375a764b3d967c54bd0
pet0cek <152738828+pet0cek@users.noreply.github.com>
2024-01-13 21:22:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var status = 1;  // 1 - online, 7 - invisible
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => )){
+user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
 	user.gamesPlayed(games);
